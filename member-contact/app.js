2cd924631e59a49458b82ab27f57e249adda8ec0
capn <jamesgreen.311@gmail.com>
2021-02-20 11:45:15 -0500

    UI Changes - wait message - button spacing

--- member-contact/app.js
+++ member-contact/app.js
@@ -1,7 +1,7 @@
 function getEmailList() {
     const url = "https://script.google.com/macros/s/AKfycbw7LsL3ASCbX82jmKa0K1_P66Lz8mBTqh5LLJbpxktF4GR4shm8qBLYig/exec";
     let msg = document.getElementById('msg');
-    msg.innerHTML = "Please wait ...";
+    msg.innerHTML = "Fetching, Please wait ...";
 
     fetch(url)
         .then(d => d.json())
