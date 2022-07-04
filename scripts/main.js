798bbeac963758141387662bffed0586b7df3708
james green <jamesgreen.311@gmail.com>
2022-07-04 16:14:57 -0400

    Add Total Exhibiting Members

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
-// Endpoint => Members Dues Project, version 8
+// Endpoint => Members Dues Project, version 10
 const EP_MEMBERS_DUES = 
-    "https://script.google.com/macros/s/AKfycbw9FU5odEsod-PY2T_jsJMqDzbMIJEPWrWC-a1jyPCDcmUy6Sw4BEMAGV8akY5Jq-Rw/exec/exec" +
+    "https://script.google.com/macros/s/AKfycbzGVVEYcXe_nlTESK9YWwsYr4jtSXowoIpJe-9t5fxFOAIgjdJ9FWjXXnXvItTIASlW/exec" +
     "?q="
 
 function fetchTotalMembers() {
@@ -14,8 +14,24 @@ function fetchTotalMembers() {
         .catch()
 }
 
+function fetchTotalExhibitingMembers() {
+    const url = EP_MEMBERS_DUES + "totalexhibitingmembers"
+    fetch(url)
+        .then(resp => resp.json())
+        .then(resp => {
+            t = resp
+            showTotalExhibitingMembers(t)
+        })
+        .catch()
+}
+
 function showTotalMembers(t) {
     let ele = document.getElementById('memberCount')
+    ele.append(t)
+}
+
+function showTotalMembers(t) {
+    let ele = document.getElementById('exhibitingMemberCount')
     ele.append(t)
 }
 
