faa0dcbac980e6569e2fd1f6effb606bb6330c3a
james green <jamesgreen.311@gmail.com>
2022-07-04 14:41:37 -0400

    Get Total Members Count

---
+++ scripts/main.js
@@ -0,0 +1,22 @@
+// Endpoint => Members Dues Project, version 8
+const EP_MEMBERS_DUES = 
+    "https://script.google.com/macros/s/AKfycbw9FU5odEsod-PY2T_jsJMqDzbMIJEPWrWC-a1jyPCDcmUy6Sw4BEMAGV8akY5Jq-Rw/exec/exec" +
+    "q="
+
+function fetchTotalMembers() {
+    const url = ENDPOINT + "totalmembers"
+    fetch(url)
+        .then(resp => resp.json())
+        .then(resp => {
+            t = resp
+            showTotalMembers(t)
+        })
+        .catch()
+}
+
+function showTotalMembers(t) {
+    let ele = document.getElementById('memberCount')
+    ele.append(t)
+}
+
+document.addEventListener("DOMContentLoaded", fetchTotalMembers)
